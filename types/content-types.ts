c2a035df87545eae07adad6efe1c0a4ba1071912
Rizwan <expectp@gmail.com>
2026-05-15 04:47:17 +0500

    centralized interface definitions and removed unused props

---
+++ types/content-types.ts
@@ -0,0 +1,41 @@
+import type { BlockEditorNode, Contentlet } from "@dotcms/types";
+
+export interface BannerFields {
+	title: string;
+	image?: string;
+	mobileImage?: string;
+	detail?: string;
+	articleSlug?: string;
+	issue?: IssueContentlet[];
+}
+
+export interface FooterContentFields {
+	title?: string;
+	content?: BlockEditorNode;
+	showNewsletterLinks?: boolean | string;
+}
+
+export interface IssueFields {
+	title?: string;
+	slug?: string;
+	publishDate?: string;
+	expireDate?: string;
+	image?: string;
+	articles?: ArticleContentlet[];
+}
+
+export interface ArticleFields {
+	title?: string;
+	slug?: string;
+	teaser?: string;
+	image?: string;
+	mobileImage?: string;
+	heroImage?: string;
+	content?: BlockEditorNode;
+	tags?: string | string[];
+}
+
+export type BannerContentlet = Contentlet<BannerFields>;
+export type FooterContentContentlet = Contentlet<FooterContentFields>;
+export type IssueContentlet = Contentlet<IssueFields>;
+export type ArticleContentlet = Contentlet<ArticleFields>;
