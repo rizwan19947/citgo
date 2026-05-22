b1c7141cffd9dd8009ee431545123a9e87a1c9df
Rizwan <expectp@gmail.com>
2026-05-22 21:46:36 +0500

    updated next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const isDev = process.env.NODE_ENV === "development";
 const nextConfig: NextConfig = {
-	assetPrefix: isDev ? "http://localhost:3000" : undefined,
+	...(isDev ? { assetPrefix: "http://localhost:3000" } : {}),
 	allowedDevOrigins: ["*", "citgonow.com", "citgonowlubes.com", "citgoretailconnections.com"],
 	images: {
 		loader: "custom",
